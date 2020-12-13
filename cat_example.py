c8c6e45d423bf93075e7682ac87163edcbab9a52
ttzorg <73962723+ttzorg@users.noreply.github.com>
2020-12-13 10:31:47 +0300

    Add files via upload

---
+++ cat_example.py
@@ -0,0 +1,5 @@
+from cat import Cat
+cat_1 = Cat("Baron","male",2)
+cat_2 = Cat("Sam","male",2)
+print("Кот:",cat_1.getName(),cat_1.getGender(),cat_1.getAge())
+print("Кот:",cat_2.getName(),cat_2.getGender(),cat_2.getAge())
